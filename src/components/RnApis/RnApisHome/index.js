c186536ee5598ec15464034c6503257cc0c13ff8
Huu Pham <phamhuu2701@gmail.com>
2020-07-19 12:59:52 +0700

    first commit

---
+++ src/components/RnApis/RnApisHome/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+function RnApisHome(props) {
+  return (
+    <View>
+      <Text>RnApisHome</Text>
+    </View>
+  );
+}
+
+export default RnApisHome;
